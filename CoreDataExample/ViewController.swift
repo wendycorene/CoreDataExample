6472c593ed4bb518ec006da090e97029035fa188
Eloe,Wendy C <s524779@ch140029m.nwmissouri.edu>
2018-04-03 10:50:11 -0500

    Work in class

--- CoreDataExample/ViewController.swift
+++ CoreDataExample/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
@@ -20,6 +21,50 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func loadBTN(_ sender: UIButton) {
+        let moc = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let player1 = NSEntityDescription.insertNewObject(forEntityName: "Player", into: moc) as! Player
+        
+        let player2 = NSEntityDescription.insertNewObject(forEntityName: "Player", into: moc) as! Player
+        
+        let player3 = NSEntityDescription.insertNewObject(forEntityName: "Player", into: moc) as! Player
+        
+        let game1 = NSEntityDescription.insertNewObject(forEntityName: "Game", into: moc) as! Game
+        
+        let game2 = NSEntityDescription.insertNewObject(forEntityName: "Game", into: moc) as! Game
+        
+        player1.age = 17
+        player1.handle = "Geoff"
+        player2.age = 43
+        player2.handle = "STORMLORD"
+        player3.age = 83
+        player3.handle = "LAME-O"
+        
+        game1.name = "Betrayal at the House on the Hill has Eyes and is Behind You"
+        game1.time = 67.3
+        game2.name = "Red Dragon Inn 2"
+        game2.time = 35.5
+        
+        game1.addToHasPlayers(player1)
+        game1.addToHasPlayers(player2)
+        game2.addToHasPlayers(player1)
+        game2.addToHasPlayers(player3)
+        
+        do {
+            try moc.save()
+            print("Save Worked!")
+        } catch {
+            print("Save failed + \(error)")
+        }
+    }
+    
+    @IBAction func fetchPlayer1(_ sender: UIButton) {
+        let moc = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let fetcher: NSFetchRequest<Player> = NSFetchRequest(entityName: "Player")
+        
+        
+    }
 }
 
